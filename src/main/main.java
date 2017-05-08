4ad10c68d8c6ef31fd3239ddc92f99568749a0d0
张培龙 <276947850@qq.com>
2017-05-09 07:32:04 +0800

    更改ui
    替换了true等显示通的状态的指示标志，去掉了设置按钮，调整了宽度

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.swing.*;
+
 /**
  * Created by Administrator on 2017/4/17 0017.
  */
@@ -7,6 +9,7 @@ public class main {
     public static void main(String[] args){
         try {
             org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+            UIManager.put("RootPane.setupButtonVisible",false);
         } catch (Exception e) {
             e.printStackTrace();
         }
